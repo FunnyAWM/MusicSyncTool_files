1cb714e29270a43ecc9927fef2995a3bdab246de
FunnyAWM <1142978300@qq.com>
2024-12-25 18:06:17 +0800

    Add files via upload

---
+++ template.ts
@@ -0,0 +1,469 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_Hans">
+<context>
+    <name>AboutPageClass</name>
+    <message>
+        <location filename="AboutPage.ui" line="14"/>
+        <source>关于</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="AboutPage.ui" line="35"/>
+        <source>作者：FunnyAWM</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="AboutPage.ui" line="45"/>
+        <source>电子邮箱：funnyawm@qq.com</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="AboutPage.ui" line="55"/>
+        <source>GitHub:@FunnyAWM</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="AboutPage.ui" line="93"/>
+        <source>确定</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LoadingPageClass</name>
+    <message>
+        <location filename="LoadingPage.ui" line="14"/>
+        <source>正在加载</source>
+        <oldsource>正在加载，请等待...</oldsource>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MusicSyncTool</name>
+    <message>
+        <location filename="MusicSyncTool.cpp" line="111"/>
+        <location filename="MusicSyncTool.cpp" line="154"/>
+        <location filename="MusicSyncTool.cpp" line="840"/>
+        <location filename="MusicSyncTool.cpp" line="918"/>
+        <source>音量：</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MusicSyncTool.cpp" line="112"/>
+        <location filename="MusicSyncTool.cpp" line="152"/>
+        <location filename="MusicSyncTool.cpp" line="910"/>
+        <source>播放已结束。</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MusicSyncTool.cpp" line="121"/>
+        <location filename="MusicSyncTool.cpp" line="124"/>
+        <location filename="MusicSyncTool.cpp" line="127"/>
+        <location filename="MusicSyncTool.cpp" line="130"/>
+        <location filename="MusicSyncTool.cpp" line="535"/>
+        <source>错误</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MusicSyncTool.cpp" line="121"/>
+        <source>没有选定音频！（提示：可以通过双击表格中的歌曲来预览）</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MusicSyncTool.cpp" line="124"/>
+        <source>没有选定路径！</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MusicSyncTool.cpp" line="127"/>
+        <source>没有设置喜爱标签！请在设置中指定！</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MusicSyncTool.cpp" line="130"/>
+        <source>没有选定文件！</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MusicSyncTool.cpp" line="133"/>
+        <location filename="MusicSyncTool.cpp" line="136"/>
+        <location filename="MusicSyncTool.cpp" line="785"/>
+        <location filename="MusicSyncTool.cpp" line="798"/>
+        <source>提示</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MusicSyncTool.cpp" line="133"/>
+        <source>已经是第一页了！</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MusicSyncTool.cpp" line="136"/>
+        <source>已经是最后一页了！</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MusicSyncTool.cpp" line="146"/>
+        <source>暂停</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MusicSyncTool.cpp" line="149"/>
+        <location filename="MusicSyncTool.cpp" line="151"/>
+        <location filename="MusicSyncTool.cpp" line="909"/>
+        <source>播放</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MusicSyncTool.cpp" line="392"/>
+        <location filename="MusicSyncTool.cpp" line="395"/>
+        <location filename="MusicSyncTool.cpp" line="398"/>
+        <source>复制</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MusicSyncTool.cpp" line="392"/>
+        <source>失败：文件已存在</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MusicSyncTool.cpp" line="395"/>
+        <source>失败：找不到歌词文件</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MusicSyncTool.cpp" line="398"/>
+        <source>失败：磁盘已满</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MusicSyncTool.cpp" line="408"/>
+        <location filename="MusicSyncTool.cpp" line="411"/>
+        <source>加载</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MusicSyncTool.cpp" line="408"/>
+        <source>失败：文件不可扫描</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MusicSyncTool.cpp" line="411"/>
+        <source>失败：标签错误</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MusicSyncTool.cpp" line="535"/>
+        <source>无法打开设置文件</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MusicSyncTool.cpp" line="635"/>
+        <source>复制结果</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MusicSyncTool.cpp" line="638"/>
+        <source>加载结果</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MusicSyncTool.cpp" line="667"/>
+        <source>正在播放：</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MusicSyncTool.cpp" line="785"/>
+        <source>确定要清除所有日志文件吗？</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MusicSyncTool.cpp" line="798"/>
+        <source>日志文件已清除</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MusicSyncToolClass</name>
+    <message>
+        <location filename="MusicSyncTool.ui" line="20"/>
+        <source>音乐同步工具</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MusicSyncTool.ui" line="29"/>
+        <source>本地文件夹：</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MusicSyncTool.ui" line="55"/>
+        <location filename="MusicSyncTool.ui" line="89"/>
+        <source>搜索</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MusicSyncTool.ui" line="66"/>
+        <source>设备文件夹：</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MusicSyncTool.ui" line="108"/>
+        <location filename="MusicSyncTool.ui" line="145"/>
+        <source>标题</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MusicSyncTool.ui" line="113"/>
+        <location filename="MusicSyncTool.ui" line="150"/>
+        <source>艺术家</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MusicSyncTool.ui" line="118"/>
+        <location filename="MusicSyncTool.ui" line="155"/>
+        <source>专辑</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MusicSyncTool.ui" line="123"/>
+        <location filename="MusicSyncTool.ui" line="160"/>
+        <source>流派</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MusicSyncTool.ui" line="128"/>
+        <location filename="MusicSyncTool.ui" line="165"/>
+        <source>年份</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MusicSyncTool.ui" line="133"/>
+        <location filename="MusicSyncTool.ui" line="170"/>
+        <source>音轨号</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MusicSyncTool.ui" line="186"/>
+        <source>复制选中的音乐到设备</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MusicSyncTool.ui" line="193"/>
+        <location filename="MusicSyncTool.ui" line="258"/>
+        <source>刷新列表</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MusicSyncTool.ui" line="200"/>
+        <location filename="MusicSyncTool.ui" line="265"/>
+        <source>仅显示喜爱的音乐</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MusicSyncTool.ui" line="227"/>
+        <location filename="MusicSyncTool.ui" line="292"/>
+        <source>上一页</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MusicSyncTool.ui" line="234"/>
+        <location filename="MusicSyncTool.ui" line="299"/>
+        <source>下一页</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MusicSyncTool.ui" line="251"/>
+        <source>复制选中的音乐到本地</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MusicSyncTool.ui" line="312"/>
+        <source>播放</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MusicSyncTool.ui" line="363"/>
+        <source>音量</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MusicSyncTool.ui" line="400"/>
+        <source>文件</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MusicSyncTool.ui" line="404"/>
+        <source>打开文件夹</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MusicSyncTool.ui" line="416"/>
+        <source>音乐</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MusicSyncTool.ui" line="420"/>
+        <source>扫描重复音乐</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MusicSyncTool.ui" line="429"/>
+        <source>日志</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MusicSyncTool.ui" line="440"/>
+        <source>退出</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MusicSyncTool.ui" line="445"/>
+        <source>本地文件夹</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MusicSyncTool.ui" line="450"/>
+        <source>设备文件夹</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MusicSyncTool.ui" line="455"/>
+        <source>关于</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MusicSyncTool.ui" line="460"/>
+        <source>本地</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MusicSyncTool.ui" line="465"/>
+        <source>设备</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MusicSyncTool.ui" line="470"/>
+        <source>设置</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MusicSyncTool.ui" line="475"/>
+        <source>清理日志文件</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>OperationResultClass</name>
+    <message>
+        <location filename="OperationResult.ui" line="14"/>
+        <source>复制结果</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="OperationResult.ui" line="44"/>
+        <source>确定</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SettingsClass</name>
+    <message>
+        <location filename="Settings.ui" line="14"/>
+        <source>设置</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Settings.ui" line="22"/>
+        <source>自定义“喜欢的音乐”标签：</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Settings.ui" line="35"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;用于指定由播放器定义的“我喜欢的音乐”的特殊音频标签。若无可不填写，但是会影响喜欢的音乐的筛选。&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Settings.ui" line="96"/>
+        <source>排序依据：</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Settings.ui" line="103"/>
+        <source>标题</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Settings.ui" line="116"/>
+        <source>艺术家</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Settings.ui" line="126"/>
+        <source>专辑</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Settings.ui" line="143"/>
+        <source>排序方式：</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Settings.ui" line="150"/>
+        <source>升序</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Settings.ui" line="163"/>
+        <source>降序</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Settings.ui" line="244"/>
+        <source>确定</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Settings.ui" line="251"/>
+        <source>取消</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Settings.ui" line="182"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;打开此选项将禁止程序检查外部lrc文件和歌词标签。如果您不需要程序复制歌词，请打开此选项。&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Settings.ui" line="185"/>
+        <source>忽略歌词</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Settings.ui" line="198"/>
+        <source>语言：</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ShowDupeClass</name>
+    <message>
+        <location filename="ShowDupe.ui" line="14"/>
+        <source>重复文件扫描结果</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ShowDupe.ui" line="34"/>
+        <source>重复的文件列表：</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ShowDupe.ui" line="65"/>
+        <source>确定</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ShowDupe.ui" line="87"/>
+        <source>注意：可能存在误报，该结果仅供参考</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
